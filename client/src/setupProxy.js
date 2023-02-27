70a872f00f0fd1ce30b98b673f5366e64eb2ecf8
yuurichan <superkenjo2015@gmail.com>
2023-02-27 19:13:45 +0700

    Added proxy to package.json - removed setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,12 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
+// module.exports = function(app) {
+//   app.use(
     
-    createProxyMiddleware('/api',{
-      target: 'https://auto-attend-api.onrender.com/',
-      changeOrigin: true,
-    })
-  );
-};
+//     createProxyMiddleware('/api',{
+//       target: 'https://auto-attend-api.onrender.com/',  // For deployment
+//       //target: 'http://localhost:5000',  // Keeping this for testing purposes
+//       changeOrigin: true,
+//     })
+//   );
+// };
